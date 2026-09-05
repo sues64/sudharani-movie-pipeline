c8b19f5fed796ea4269ded9d37cb43ac790cf139
sues64 <sudharani1042004@gmail.com>
2026-09-05 06:57:55 +0000

    Updated spell Mistakes

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,19 +6,26 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      const movieList = response.data.movies || response.data || [];
-      setMovies(Array.isArray(movieList) ? movieList : []);
-    }).catch((error) => {
-      console.error("Error Fetching movies:", error);
-      setMovies([]);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        const movieList = response.data.movies || response.data || [];
+        setMovies(Array.isArray(movieList) ? movieList : []);
+      })
+      .catch((error) => {
+        console.error('Error Fetching movies:', error);
+        setMovies([]);
+      });
   }, []);
 
   return (
     <ul>
       {movies?.map((movie) => (
-        <li className="movieItem" key={movie.id || movie._id} onClick={() => onMovieClick(movie)}>
+        <li
+          className="movieItem"
+          key={movie.id || movie._id}
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
